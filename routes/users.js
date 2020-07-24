65b37a279fac5ca735b40d1cf8f3ee8b63b692be
abhinavchawla07 <abhinavchawla07@gmail.com>
2020-07-24 20:38:08 +0530

    Passport

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var Users = require('../models/users');
+var passport = require('passport');
 
 var usersRouter = express.Router();
 
@@ -11,69 +12,26 @@ usersRouter.get('/', function (req, res, next) {
 });
 
 usersRouter.post('/signup', (req, res, next) => {
-	Users.findOne({ username: req.body.username })
-		.then((user) => {
-			if (user != null) {
-				var err = new Error('User ' + req.body.username + ' already exists');
-				err.status = 403;
-				next(err);
-			}
-			else {
-				return Users.create({
-					username: req.body.username,
-					password: req.body.password
-				});
-			}
-		})
-		.then((user) => {
-			res.statusCode = 200;
+	Users.register(new Users({ username: req.body.username }), req.body.password, (err, user) => {
+		if (err) {
+			res.statusCode = 500;
 			res.setHeader('Content-Type', 'application/json');
-			res.json({ status: 'registration successful', user: user });
-		}, (err) => next(err))
-		.catch((err) => next(err));
+			res.json({ err: err });
+		}
+		else {
+			passport.authenticate('local')(req, res, () => {
+				res.statusCode = 200;
+				res.setHeader('Content-Type', 'application/json');
+				res.json({ status: 'Registration Successful', success: true });
+			});
+		}
+	});
 });
 
-usersRouter.post('/login', (req, res, next) => {
-	if (!req.session.user) {
-		var authHeader = req.headers.authorization;
-		if (!authHeader) {
-			var err = new Error('You are not authenticated');
-
-			res.setHeader('WWW-Authenticate', 'Basic');
-			err.status = 401;
-			return next(err);
-		}
-
-		var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-		var username = auth[0];
-		var password = auth[1];
-
-		Users.findOne({ username: username })
-			.then((user) => {
-				if (user === null) {
-					var err = new Error('User ' + username + ' does not exist');
-					err.status = 401;
-					return next(err);
-				}
-				else if (user.password !== password) {
-					var err = new Error('Incorrect Password');
-					err.status = 401;
-					return next(err);
-				}
-				else if (user.username === username && user.password === password) {
-					req.session.user = 'authenticated';
-					res.statusCode = 200;
-					res.setHeader('Content-Type', 'text/plain');
-					res.end('You are authenticated');
-				}
-			})
-			.catch((err) => next(err));
-	}
-	else {
-		res.statusCode = 200;
-		res.setHeader('Content-Type', 'text/plain');
-		res.end('You are already authenticated');
-	}
+usersRouter.post('/login', passport.authenticate('local'), (req, res, next) => {
+	res.statusCode = 200;
+	res.setHeader('Content-Type', 'application/json');
+	res.json({ status: 'Login Successful', success: true });
 });
 
 usersRouter.get('/logout', (req, res, next) => {
