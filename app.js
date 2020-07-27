8af6b5f6f98b9c2870f88ad484644756897725f0
abhinavchawla07 <abhinavchawla07@gmail.com>
2020-07-28 01:33:26 +0530

    File Upload

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var leaderRouter = require('./routes/leaderRouter');
 var promoRouter = require('./routes/promoRouter');
+var uploadRouter = require('./routes/uploadRouter');
 
 const mongoose = require('mongoose');
 
@@ -53,25 +54,13 @@ app.use(passport.initialize());
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// function auth(req, res, next) {
-// 	if (!req.user) {
-// 		var err = new Error('You are not authenticated');
-// 		res.setHeader('WWW-Authenticate', 'Basic');
-// 		err.status = 403;
-// 		return next(err);
-// 	}
-// 	else {
-// 		next();
-// 	}
-// }
-
-// app.use(auth);
 app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
+app.use('/imageUpload', uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
